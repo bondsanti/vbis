4be68c59088a66ecfe8301a0088a71e6083d13ed
santi.c <santi.chpy@gmail.com>
2024-06-19 10:53:47 +0700

    update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,107 +4,140 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redis;
 use RealRashid\SweetAlert\Facades\Alert;
 
 class ApiController extends Controller
 {
 
 
-   public function getAuth(Request $request, $code)
-   {
+    public function getAuth(Request $request, $code)
+    {
 
-       $user = User::where('code', $code)->where('active',1)->first();
+        $user = User::where('code', $code)->where('active', 1)->first();
 
-       if (!$user) {
-           return response()->json(['message' => 'ไม่พบผู้ใช้งานระบบ'], 404);
-       }
+        if (!$user) {
+            return response()->json(['message' => 'ไม่พบผู้ใช้งานระบบ'], 404);
+        }
 
-       return response()->json(['data' => $user], 200);
-   }
-
-
-   public function createUserbyHR(Request $request)
-   {
-    $userData = $request->all();
-    $user = User::create($userData);
-    if ($user) {
-        return response()->json(['message' => 'User created successfully'], 200);
-    } else {
-        return response()->json(['message' => 'Failed to create user'], 500);
+        return response()->json(['data' => $user], 200);
     }
 
-   }
 
-   //CheckToken
-   public function checkTokenLogin(Request $request, $token)
-   {
+    public function createUserbyHR(Request $request)
+    {
+        $userData = $request->all();
+        $user = User::create($userData);
+        if ($user) {
+            return response()->json(['message' => 'User created successfully'], 200);
+        } else {
+            return response()->json(['message' => 'Failed to create user'], 500);
+        }
+    }
 
-       $user = User::where('token', $token)->first();
+    //CheckToken
+    public function checkTokenLogin(Request $request, $token)
+    {
+
+        $user = User::where('token', $token)->select('token')->first();
 
 
-       if (!$user) {
-           return response()->json(['message' => 'ไม่พบผู้ใช้งานระบบ'], 404);
-       }
+        if (!$user) {
+            return response()->json(['message' => 'ไม่พบผู้ใช้งานระบบ'], 404);
+        }
 
-       return response()->json(['data' => $user->token], 200);
-   }
+        return response()->json(['data' => $user->token], 200);
+    }
+
+    public function getRoleUserAll($user_ids)
+    {
+
+        $userIdsArray = explode(',', $user_ids);
+
+        $users = User::whereIn('user_id', $userIdsArray)
+            ->select('user_id', 'code', 'email', 'token_forget', 'active', 'low_rise', 'high_rise', 'active_agent', 'active_vblead', 'active_vproject')
+            ->get();
+
+        if ($users->isEmpty()) {
+            return response()->json(['message' => 'ไม่พบผู้ใช้งานระบบ'], 404);
+        }
+
+        return response()->json(['data' => $users], 200);
+    }
 
     //CheckTokenPublicWeb
-   public function checkTokenPublicSite(Request $request, $token)
-   {
+    public function checkTokenOut($token)
+    {
 
-       $user = User::where('token', $token)->first();
+        $user = User::where('token', $token)
+            ->select(
+                'user_id',
+                'code',
+                'email',
+                'token',
+                'token_forget',
+                'is_auth',
+                'active',
+                'low_rise',
+                'high_rise',
+                'active_agent',
+                'active_vblead',
+                'active_vproject'
+            )->first();
 
+        if (!$user) {
+            return response()->json(['message' => 'ไม่พบผู้ใช้งานระบบ'], 404);
+        }
 
-       if (!$user) {
-           return response()->json(['message' => 'ไม่พบผู้ใช้งานระบบ'], 404);
-       }
+        $this->addApiDataToUser($user);
 
-       return response()->json(['data' => $user], 200);
-   }
+        return response()->json(['data' => $user], 200);
+    }
 
-   private function addApiDataToUsers($users)
-   {
-       $client = new Client();
-       $apiUrl = config('services.external_api.url');
-       $apiToken = config('services.external_api.token');
+    private function addApiDataToUser($user)
+    {
+        $client = new Client();
+        $apiUrl = config('services.external_api.url');
+        $apiToken = config('services.external_api.token');
 
-       foreach ($users as $user) {
-           try {
-               $response = $client->request('GET', $apiUrl.'/users', [
-                   'query' => ['user_id' => $user->id],
-                   'headers' => [
-                       'Authorization' => 'Bearer ' . $apiToken
-                   ]
-               ]);
+        try {
+            $response = $client->request('GET', $apiUrl . '/users', [
+                'query' => ['user_id' => $user->user_id],
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $apiToken
+                ]
+            ]);
 
-               $user->apiData = json_decode($response->getBody(), true);
+            $apiData = json_decode($response->getBody(), true);
+            $user->apiData = $apiData;
 
-               $imgCheck = optional(optional($user->apiData)['data'])['img_check'];
-               $remoteFile = $imgCheck ? "http://vbhr.vbeyond.co.th/imageUser/employee/{$imgCheck}" : null;
-               //$remoteFile = $imgCheck ? "http://localhost/hr/imageUser/employee/{$imgCheck}" : null;
-               $fileExists = false;
+            // Check for image existence
+            $imgCheck = optional(optional($apiData)['data'])['img_check'];
+            $remoteFile = $imgCheck ? "http://localhost/hr/imageUser/employee/{$imgCheck}" : null;
+            //$remoteFile = $imgCheck ? "http://vbhr.vbeyond.co.th/imageUser/employee/{$imgCheck}" : null;
+            $fileExists = $this->checkRemoteFileExists($remoteFile);
 
-               if ($remoteFile) {
-                   $ch = curl_init($remoteFile);
-                   curl_setopt($ch, CURLOPT_NOBODY, true);
-                   curl_exec($ch);
-                   $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-                   curl_close($ch);
+            $user->remoteFile = $remoteFile;
+            $user->fileExists = $fileExists;
+        } catch (\Exception $e) {
+            // Log::error('API request failed for user ' . $user->id . ': ' . $e->getMessage());
+            $user->apiData = null;
+        }
+    }
 
-                   $fileExists = ($responseCode == 200);
-               }
+    private function checkRemoteFileExists($url)
+    {
+        if (!$url) {
+            return false;
+        }
 
-               $user->remoteFile = $remoteFile;
-               $user->fileExists = $fileExists;
-
-           } catch (\Exception $e) {
-
-               //Log::error('API request failed for user ' . $user->id . ': ' . $e->getMessage());
-               $user->apiData = null;
-           }
-       }
-   }
-
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_NOBODY, true);
+        curl_exec($ch);
+        $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        return ($responseCode == 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,13 @@ Route::middleware(['checkTokenApi'])->group(function () {
     Route::get('/checktoken/{token}', [ApiController::class, 'checkTokenLogin'])->name('login.checkToken');
 
     // Allow Login Public App
-    Route::get('/checktoken/publicsite/{token}', [ApiController::class, 'checkTokenPublicSite'])->name('login.checkTokenPublic');
+    Route::get('/checktoken/out/{token}', [ApiController::class, 'checkTokenOut'])->name('login.checkTokenOut');
+
+    // Insert Log Login Form Publib App To DB Report
+    Route::get('/create/login/log', [ApiController::class, 'getAuth'])->name('auth.getAuth');
+
+    Route::get('/get-role/users/{user_ids}',[ApiController::class,'getRoleUserAll']);
+
 });
 
 
